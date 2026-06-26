0396f72a7c39e9ae47e7b0058877c06a8c4c99bc
Derek <realdiriktv@gmail.com>
2026-06-25 21:37:22 -0500

    Bump version for Ko-fi modal placement

--- simpcity-hide-threads.meta.js
+++ simpcity-hide-threads.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SimpCity Hide Threads
 // @namespace    https://github.com/vylix-dev/simpcity-hide-threads
-// @version      1.1.1
+// @version      1.1.2
 // @description  Persistently hide SimpCity threads and manage your hidden-thread list.
 // @author       vylix-dev
 // @license      MIT
